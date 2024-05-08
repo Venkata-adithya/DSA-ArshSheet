0216bdc0f21da8b86370983e54757e755c857bd5
Venkata-adithya <113289169+Venkata-adithya@users.noreply.github.com>
2024-05-08 08:09:08 +0530

    Create ValidPalindrome2.java

---
+++ ValidPalindrome2.java
@@ -0,0 +1,30 @@
+
+//LC-680
+class Solution {
+    public boolean validPalindrome(String s) {
+        int l=0;
+        int r=s.length()-1;
+        while(l<=r){
+            if(s.charAt(l)==s.charAt(r)){
+                l++;
+                r--;
+            }
+            else{
+                return isPalindrome(s,l+1,r) || isPalindrome(s,l,r-1);
+            }
+        }
+        return true;
+    }
+    public static boolean isPalindrome(String s,int l,int r){
+        while(l<=r){
+            if(s.charAt(l)==s.charAt(r)){
+                l++;
+                r--;
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+}
