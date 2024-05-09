2db675b4473733b8fe0c715af71fe758b1678ca2
Venkata-adithya <113289169+Venkata-adithya@users.noreply.github.com>
2024-05-09 10:46:52 +0530

    Create MinMovesEqual.java

---
+++ MinMovesEqual.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minMoves(int[] nums) {
+        int res=0;
+        int minval=Integer.MAX_VALUE;
+        for(int i=0;i<nums.length;i++){
+            minval=Math.min(nums[i],minval);
+        }
+        for(int i=0;i<nums.length;i++){
+            res=res+(nums[i]-minval);
+        }
+        return res;
+    }
+}
